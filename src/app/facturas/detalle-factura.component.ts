5f1f7eefb1262d93ca93bcbacea16f424c7d370f
musiciancoder <andresguitarra1980@gmail.com>
2020-07-07 16:02:45 -0400

    168, no funciona

--- src/app/facturas/detalle-factura.component.ts
+++ src/app/facturas/detalle-factura.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FacturaService} from './services/factura.service';
+import {Factura} from './models/factura';
+import {ActivatedRoute} from '@angular/router';
+
 
 @Component({
   selector: 'app-detalle-factura',
@@ -7,9 +11,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetalleFacturaComponent implements OnInit {
 
-  constructor() { }
+  factura: Factura;
+  titulo: string = 'Factura';
+
+  constructor(private facturasService:FacturaService,
+              private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.activatedRoute.paramMap.subscribe(params =>{ //obtener el id de la ruta (sin llamada al backend)
+      let id = +params.get('id');
+      this.facturasService.getFactura(id).subscribe(factura=>this.factura = factura)
+
+    })
   }
 
 }
